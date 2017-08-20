5ec72f4dc1110d92227e5ace10b7c4f9c558d86c
Sahil <sahilhaque24.10@gmail.com>
2017-08-20 20:40:34 +0530

    Completed adding, Styling functioning the modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import $ from 'jquery';
 import MobileMenu from './modules/_MobileMenu';
 import RevealOnScroll from './modules/_RevealOnScroll';
 import StickyHeader from './modules/_StickyHeader';
+import Modal from './modules/_Modal';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '80%');
 new RevealOnScroll($('.testimonial'), '60%');
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
