362f1b00714d7e4119dfdb38c614cca401809190
Sahil <sahilhaque24.10@gmail.com>
2017-08-20 14:09:42 +0530

    Completed adding reveal on scroll effect

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/_MobileMenu';
+import RevealOnScroll from './modules/_RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($('.feature-item'), '80%');
+new RevealOnScroll($('.testimonial'), '60%');
