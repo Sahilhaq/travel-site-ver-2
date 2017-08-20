dffd30482067173cc82813280b7180530b856cb6
Sahil <sahilhaque24.10@gmail.com>
2017-08-20 17:59:02 +0530

    Completed StickyHeader and implementing smothScrolling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/_MobileMenu';
 import RevealOnScroll from './modules/_RevealOnScroll';
+import StickyHeader from './modules/_StickyHeader';
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($('.feature-item'), '80%');
 new RevealOnScroll($('.testimonial'), '60%');
+var stickyHeader = new StickyHeader();
